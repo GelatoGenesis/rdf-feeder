b7959cd6195d848f1d35cfbf9ed0715fc63718d8
Tarmo Kalling <tarmo.kalling@nortal.com>
2015-05-02 14:59:33 +0300

    Extra debug line.

--- src/main/java/com/rdffeeder/RDFFeeder.java
+++ src/main/java/com/rdffeeder/RDFFeeder.java
@@ -10,12 +10,19 @@ import polimi.deib.csparql_rest_api.RSP_services_csparql_API;
 import polimi.deib.csparql_rest_api.exception.ServerErrorException;
 import polimi.deib.csparql_rest_api.exception.StreamErrorException;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
+
 public class RDFFeeder {
 
     private static final Logger logger = LoggerFactory.getLogger(RDFFeeder.class);
 
     public static final String PROPERTY_FILE_NAME = "rdffeeder.properties";
     public static final String RDF_ENGINE_ENDPOINT_PROPERTY = "rdfengineEndpoint";
+    public static final String XML_HEADER_TAG = "<?xml version=\"1.0\"?>";
+    public static final String RDF_ROOT_END_TAG = "</rdf:RDF>";
 
     private RSP_services_csparql_API rdfEngine;
     private String rdfStreamName;
@@ -40,13 +47,40 @@ public class RDFFeeder {
         rdfEngine = new RSP_services_csparql_API(properties.getString(RDF_ENGINE_ENDPOINT_PROPERTY));
     }
 
-    public void feed(String rdfFilePath) throws ServerErrorException, StreamErrorException {
+    public void feedFile(String rdfFilePath) throws ServerErrorException, StreamErrorException {
         logger.info("Reading in file {} for feeding.", rdfFilePath);
         Model rdfModel = ModelFactory.createDefaultModel();
         rdfModel.read(rdfFilePath);
 
-        logger.info("Starting to feed the RDF engine.");
+        logger.info("Starting to feedFile the RDF engine.");
         rdfEngine.feedStream(rdfStreamName, rdfModel);
         logger.info("Feeding {} to stream {} successfully completed.", rdfFilePath, rdfStreamName);
+    }
+
+    public void feedString(String rdf) throws Exception {
+        logger.info("Starting to feed RDF from a string to stream {}.", rdfStreamName);
+        Model rdfModel = ModelFactory.createDefaultModel();
+        rdfModel.read(new ByteArrayInputStream(rdf.getBytes(StandardCharsets.UTF_8)), "");
+
+        rdfEngine.feedStream(rdfStreamName, rdfModel);
+        logger.info("Feeding RDF string to stream {} successfully completed.", rdfStreamName);
+    }
+
+
+    public void feedMultiRootFile(String rdfFilePath) throws Exception {
+        logger.info("Starting to process and feed a multiroot file which has no xml tag at the beginning.");
+        try (BufferedReader br = new BufferedReader(new FileReader(rdfFilePath))) {
+            StringBuilder rdf = new StringBuilder();
+            for(String line; (line = br.readLine()) != null; ) {
+                if (rdf.length() == 0) {
+                    rdf.append(XML_HEADER_TAG);
+                }
+                rdf.append(line);
+                if (line.contains(RDF_ROOT_END_TAG)) {
+                    feedString(rdf.toString());
+                    rdf = new StringBuilder();
+                }
+            }
+        }
     }
 }
--- src/main/java/com/rdffeeder/RDFFeederExecutor.java
+++ src/main/java/com/rdffeeder/RDFFeederExecutor.java
@@ -16,14 +16,64 @@ public class RDFFeederExecutor {
 
             logger.info("RDF streamer takes two mandatory parameters. First one is the " +
                     "path to the RDF file to be sent and the second one is the stream name " +
-                    "(stream with the given name will be created if it doesn't exist already).");
+                    "(stream with the given name will be created if it doesn't exist already). " +
+                    "Third parameter is optional. It represents the mode in which the feeder works. " +
+                    "Default mode will assume that the input file is valid XML with a single root RDF element. " +
+                    "Mode 2 assumes that the file doesnt have <?xml ... header and may contain multiple " +
+                    "RDF elements.");
             return;
         }
 
-
         String rdfFilePath = args[0];
         String streamName = args[1];
-        new RDFFeeder(streamName).feed(rdfFilePath);
+
+        RDFFeeder rdfFeeder = new RDFFeeder(streamName);
+
+        RDFFeederExecutionRegime regime = RDFFeederExecutionRegime.FEED_VALID_XML_FILE_WITH_SINGLE_RDF; // default regime if one is not given as input.
+
+        if (args.length >= 3) {
+            regime = RDFFeederExecutionRegime.fromString(args[2]);
+        }
+
+        switch (regime) {
+            case MULTIPLE_ROOT_RDF_ELEMENTS_XML_HEADER_MISSING:
+                rdfFeeder.feedMultiRootFile(rdfFilePath);
+                break;
+            default:
+                rdfFeeder.feedFile(rdfFilePath);
+        }
+        if (regime == RDFFeederExecutionRegime.FEED_VALID_XML_FILE_WITH_SINGLE_RDF) {
+            rdfFeeder.feedFile(rdfFilePath);
+        }
+
+    }
+
+    enum RDFFeederExecutionRegime {
+        FEED_VALID_XML_FILE_WITH_SINGLE_RDF(1),
+        MULTIPLE_ROOT_RDF_ELEMENTS_XML_HEADER_MISSING(2);
+
+        private  int regimeAsint;
+
+        RDFFeederExecutionRegime(int regimeAsInt) {
+            this.regimeAsint = regimeAsInt;
+        }
+
+        static RDFFeederExecutionRegime fromInt(int regimeAsint) {
+            for (RDFFeederExecutionRegime regime : RDFFeederExecutionRegime.values()) {
+                if (regime.regimeAsint == regimeAsint) {
+                    return regime;
+                }
+            }
+            throw new RuntimeException("Invalid regime: " + regimeAsint);
+        }
+
+        static RDFFeederExecutionRegime fromString(String regimeNo) {
+            try {
+                return fromInt(Integer.parseInt(regimeNo));
+            } catch (Exception e) {
+                throw new RuntimeException("Invalid regime: " + regimeNo);
+            }
+        }
     }
 
 }
